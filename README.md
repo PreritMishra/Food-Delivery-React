265e6659aff0ebf6b81e2cddf3e6070add1a621c
Prerit <preritmishra1002@gmail.com>
2023-11-27 13:26:39 +0530

    Day2 - parcel,packages

---
+++ README.md
@@ -0,0 +1,22 @@
+# React
+
+## Parcel
+- Dev Build
+- Local Server
+- HMR- Hot Module Reload
+- File watching algo - written in C++
+- Caching - Faster Build
+- Image Optimization
+- Minification
+- Compress
+- Bundling
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Different dev and prod bundlers
+
+ 
--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const heading = React.createElement(
     "div",
     {id: "child"},
-    [React.createElement("h1", {}, "Hello Brother"), 
+    [React.createElement("h1", {}, "Hello Nhi RAM RAM BHAI"), 
     React.createElement("h1", {}, "RAM RAM BHAI")]
-);   
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
